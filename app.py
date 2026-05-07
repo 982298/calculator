fa47ebd881cb7af8b1244fedc2545178da50a3e4
982298 <gamifiedlearnig.pvt.ltd@gmail.com>
2026-05-07 15:35:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -26,7 +27,8 @@ def calculator():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
